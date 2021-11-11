8c2e3d8e574f62597e5f7171883640cfcb18f0f9
john gallagher <127highstreet@gmail.com>
2021-11-11 18:36:39 -0500

    saving

--- scripts/guests/GuestCard.js
+++ scripts/guests/GuestCard.js
@@ -1,12 +1,18 @@
+import { GuestEditForm } from "./GuestEditForm.js"
+
 export const guest = (taco) => {
     return `
     <div class="guest-list-group-item  ${taco.id}">
     <h4>Guest Name: ${taco.name}</h4>
     <img src="${taco.imageUrl}" alt="${taco.name}" class="guest-imgs" width="50vw"
     </img>
-    <p>Favorite Dish: ${taco.favoriteDish}</p>
-    <p>Age: ${taco.age}</p>
-    <p>Right Handed: ${taco.rightHanded}</p>
+    <p class="guest-dish">Favorite Dish: ${taco.favoriteDish}</p>
+    <p class="guest-age">Age: ${taco.age}</p>
+    ${taco.rightHanded ?
+    `<p class="guest-righty">Righty</p>
+    `: 
+    `<p class="guest-lefty">Lefty</p>
+    ` }
     <button id="deleteGuest--${taco.id}">Delete</button>
     <button id="edit--${taco.id}">Edit</button>
     </div>
@@ -15,3 +21,11 @@ export const guest = (taco) => {
 //things to do 
 // 1. check the data structer so that it is printing
 // 2. tweak for img tag at W3
+//<p>Right Handed: ${taco.rightHanded}</p>
+
+const eventHub = document.querySelector(".current-guest-list")
+eventHub.addEventListener("click", (eventObject) => {
+
+    const guestId = +eventObject.target.id.split("--")[1]
+    GuestEditForm(guestId);
+})
--- scripts/guests/GuestDataProvider.js
+++ scripts/guests/GuestDataProvider.js
@@ -30,3 +30,4 @@ export const deleteGuest =(guestId) => {
         method: "DELETE"
     })
 }
+
